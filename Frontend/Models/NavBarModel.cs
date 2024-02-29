641d9225e445da379ed34c042d5d06ef995595c7
leonTech254 <martinleontech23@gmail.com>
2024-02-29 14:04:04 +0300

    --navbar -c

---
+++ Frontend/Models/NavBarModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frontend.Models
+{
+    public class NavBarModel
+    {
+        public string navName{ get;set;}
+        public string navLink {get;set;}
+    }
+}
