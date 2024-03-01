46f2c05cdb19998ec1d688a8d931653c89ea764c
leonTech254 <martinleontech23@gmail.com>
2024-03-01 13:14:49 +0300

    --linked backed Api

--- Frontend/Services/EventService.cs
+++ Frontend/Services/EventService.cs
@@ -1,66 +1,65 @@
 using System;
 using System.Collections.Generic;
+using System.Text.Json;
 using Frontend.Models; 
 namespace Frontend.Services
 {
     public class EventService
     {
-        public List<EventModel> Events { get; private set; }
+        public String _baseURL;
+        // public List<EventModel> Events { get; private set; }
+        public HttpClient _client;
 
         public EventService()
         {
-            // Initialize with some dummy data
-            Events = new List<EventModel>
-            {
-                new EventModel
-                {
-                    EventDate = DateTime.Now.AddDays(1),
-                    EventName = "Event 1",
-                    EventImageUrl = "https://example.com/image1.jpg",
-                    EventDescription = "Description of Event 1"
-                },
-                new EventModel
-                {
-                    EventDate = DateTime.Now.AddDays(2),
-                    EventName = "Event 2",
-                    EventImageUrl = "https://example.com/image2.jpg",
-                    EventDescription = "Description of Event 2"
-                },
-                new EventModel
-                {
-                    EventDate = DateTime.Now.AddDays(3),
-                    EventName = "Event 3",
-                    EventImageUrl = "https://example.com/image3.jpg",
-                    EventDescription = "Description of Event 3"
-                }
-            };
+        //    _httpContent= new HttpClient();
+             _client=new HttpClient(); 
+             _baseURL="http://localhost:5215/api/v1/";      
+           
         }
 
-        public List<EventModel> getAllEvents()
+        public async Task<List<EventModel>> getAllEvents()
         {
-                return Events;
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+           var response= await _client.GetAsync(_baseURL+"events");
+           if(response.IsSuccessStatusCode)
+           {
+            var jsonString=await response.Content.ReadAsStringAsync();
+            var responseDTO=JsonSerializer.Deserialize<ResponseDTO>(jsonString,options);
+            List<EventModel> events=JsonSerializer.Deserialize<List<EventModel>>(responseDTO.responseData.ToString(),options);
+            Console.WriteLine(responseDTO.responseData.ToString() +"i am here");
+            return events;
+           }else
+           {
+            return null;
+           }
+
+                
         }
 
         public void AddEvent(EventModel newEvent)
         {
-            Events.Add(newEvent);
+            // Events.Add(newEvent);
         }
 
-        public void UpdateEvent(EventModel updatedEvent)
-        {
-            var existingEvent = Events.Find(e => e.EventDate == updatedEvent.EventDate);
-            if (existingEvent != null)
-            {
-                existingEvent.EventName = updatedEvent.EventName;
-                existingEvent.EventImageUrl = updatedEvent.EventImageUrl;
-                existingEvent.EventDescription = updatedEvent.EventDescription;
-            }
-        }
+        // public void UpdateEvent(EventModel updatedEvent)
+        // {
+        //     var existingEvent = Events.Find(e => e.EventDate == updatedEvent.EventDate);
+        //     if (existingEvent != null)
+        //     {
+        //         existingEvent.EventName = updatedEvent.EventName;
+        //         existingEvent.EventImageUrl = updatedEvent.EventImageUrl;
+        //         existingEvent.EventDescription = updatedEvent.EventDescription;
+        //     }
+        // }
 
-        public void DeleteEvent(DateTime eventDate)
-        {
-            Events.RemoveAll(e => e.EventDate == eventDate);
-        }
+        // public void DeleteEvent(DateTime eventDate)
+        // {
+        //     Events.RemoveAll(e => e.EventDate == eventDate);
+        // }
     }
 
     // public class EventModel
